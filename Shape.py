8cafcd470cb34f26c3fa2eb320bef5778b049b56
brandonbk125 <78690948+brandonbk125@users.noreply.github.com>
2023-01-27 12:47:51 +0000

    added comments into shape.py

--- Shape.py
+++ Shape.py
@@ -3,14 +3,19 @@ import math
 from paintwall import get_float
 
 
+# abstract class Shape
+# blueprint for all Shape classes
 class Shape(ABC):
+    # gets the area of a shape
     def get_area(self):
         pass
 
+    # sets the dimensions of a shape
     def set_dimensions(self):
         pass
 
 
+# Rectangle class
 class Rectangle(Shape):
     def __init__(self):
         self._height = 0.0
@@ -23,13 +28,16 @@ class Rectangle(Shape):
         self._height = get_float("Enter height of rectangle (m): ")
         self._length = get_float("Enter length of rectangle (m): ")
 
+    # sets the height of a Rectangle
     def set_height(self, height: float):
         self._height = height
 
+    # sets the length of a Rectangle
     def set_length(self, length: float):
         self._length = length
 
 
+# Circle class
 class Circle(Shape):
     def __init__(self):
         self._diameter = 0.0
@@ -40,10 +48,12 @@ class Circle(Shape):
     def set_dimensions(self):
         self._diameter = get_float("Enter diameter of circle (m): ")
 
+    # sets the Diameter of a Circle
     def set_diameter(self, diameter: float):
         self._diameter = diameter
 
 
+# Triangle class
 class Triangle(Shape):
     def __init__(self):
         self._base = 0.0
@@ -56,8 +66,10 @@ class Triangle(Shape):
         self._base = get_float("Enter base of triangle (m): ")
         self._height = get_float("Enter height of triangle (m): ")
 
+    # sets the base of a Triangle
     def set_base(self, base: float):
         self._base = base
 
+    # sets the height of a Triangle
     def set_height(self, height: float):
         self._height = height
